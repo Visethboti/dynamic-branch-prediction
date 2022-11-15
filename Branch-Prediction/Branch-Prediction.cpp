ebd47cae0f0b4f3a4c42471d3b83a20654fb5307
Visethboti <visethboti@gmail.com>
2022-11-15 13:07:33 -0600

    Tested on a few example, no issues so far.

---
+++ Branch-Prediction/Branch-Prediction.cpp
@@ -0,0 +1,159 @@
+// CSCI 620 - Dynamic Branchse
+//
+
+#include <iostream>
+#include <deque>
+#include <string>
+
+using namespace std;
+
+class CorrelatingBranchPredictor {
+    public:
+        CorrelatingBranchPredictor(int theM, int theN, int theNumOfBranch) {
+            m = theM; // m num of history bit
+            n = theN; // n-bits
+            numOfBranch = theNumOfBranch;
+            bufferSizeForEachBranch = pow(2, m);
+            bufferSize = (bufferSizeForEachBranch*numOfBranch);
+            
+
+            // init all statistic to 0
+            numOfTakenBranches = 0;
+            numOfNotTakenBranches = 0;
+            numOfPredictions = 0;
+            numOfMissPredictions = 0;
+            
+            nbitCounter = 0;
+        }
+
+        void init(bool taken) {
+            // init the buffer with the given init value
+            for (int i = 0; i < bufferSize; i++)
+                buffer.push_back(taken);
+
+            // init the historyBits with the given init value
+            for (int i = 0; i < m; i++) {
+                historyBits.push_back(taken);
+                allHistoryBits.push_back(taken);
+            }
+        }
+
+        bool predict(bool actualTaken, int numOfBranch) {
+            int index = (numOfBranch-1) * 2;
+            if (m > 0) {
+                // covert historybit to binary and int to look up buffer
+                string binary = "";
+                for (int i = 0; i < m; i++) {
+                    if (historyBits[i])
+                        binary += "1";
+                    else
+                        binary += "0";
+                }
+                index += stoi(binary, 0, 2);
+            }
+            
+
+            // check buffer
+            bool predictTaken = buffer[index];
+
+            cout << "Branch predict to be " << predictTaken << endl;
+
+            numOfPredictions++;
+
+            if (predictTaken)
+                numOfTakenBranches++;
+            else
+                numOfNotTakenBranches++;
+
+            int max = (pow(2, n) - 1);
+            if (nbitCounter < max && nbitCounter < 0)
+                if (actualTaken == true)
+                    nbitCounter++;
+                else
+                    nbitCounter--;
+
+            if (predictTaken == actualTaken) { // correct prediction
+                
+            } 
+            else { // miss prediction
+                numOfMissPredictions++;
+                
+                // flip
+                if(nbitCounter >= (max/2))
+                    buffer[index] = true;
+                else
+                    buffer[index] = false;
+            }
+
+            // add to history bit
+            historyBits.push_back(actualTaken);
+            allHistoryBits.push_back(actualTaken);
+
+            historyBits.pop_front();
+
+            return predictTaken;
+        }
+
+        void printStat() {
+            double accuracy = 100-(((double)numOfMissPredictions / numOfPredictions)*100);
+            cout << "========== Statistic ==========" << endl 
+                << "Number of Taken Branches: " << numOfTakenBranches << endl
+                << "Number of Not Taken Branches: " << numOfNotTakenBranches << endl
+                << "Number of Predictions: " << numOfPredictions << endl
+                << "Number of Missed Predictions: " << numOfMissPredictions << endl
+                << "Accuracy: " << accuracy << endl
+                << "===============================" << endl;
+        }
+        
+        void printBuffer() {
+            cout << "========== Buffer ==========" << endl;
+            for (int i = 0; i < bufferSize; i++)
+                cout << buffer[i] << endl;
+        }
+
+    private:
+        int m, n, bufferSize, numOfBranch, bufferSizeForEachBranch, nbitCounter;
+        deque<bool> buffer, historyBits, allHistoryBits;
+
+        int numOfTakenBranches, numOfNotTakenBranches, numOfPredictions, numOfMissPredictions;
+};
+
+int main()
+{
+    CorrelatingBranchPredictor correlatingBranchPredictor(3,1,1);
+    correlatingBranchPredictor.init(false);
+    correlatingBranchPredictor.printBuffer();
+    /*
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(true, 1);
+    correlatingBranchPredictor.printBuffer();
+
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(true, 1);
+    correlatingBranchPredictor.printBuffer();
+    */
+
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(true, 1);
+    correlatingBranchPredictor.printBuffer();
+
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(true, 1);
+    correlatingBranchPredictor.printBuffer();
+
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(false, 1);
+    correlatingBranchPredictor.predict(true, 1);
+    correlatingBranchPredictor.printBuffer();
+
+    correlatingBranchPredictor.printStat();
+}

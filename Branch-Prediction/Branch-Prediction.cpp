ee6c078ec4b5f33623a6c6db105b1e91372638b3
Visethboti <visethboti@gmail.com>
2022-11-15 20:51:24 -0600

    Tested with base algorithm, quicksort

--- Branch-Prediction/Branch-Prediction.cpp
+++ Branch-Prediction/Branch-Prediction.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <deque>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -17,30 +18,39 @@ class DynamicBranchPredictor {
             bufferSize = (bufferSizeForEachBranch*numOfBranch);
             
 
-            // init all statistic to 0
-            numOfTakenBranches = 0;
-            numOfNotTakenBranches = 0;
-            numOfPredictions = 0;
-            numOfMissPredictions = 0;
-            
+            // init to  0
             nbitCounter = 0;
         }
 
         void init(bool taken) {
+            // init all statistic to 0
+            for (int i = 0; i < numOfBranch; i++) {
+                numOfTakenBranches.push_back(0);
+                numOfNotTakenBranches.push_back(0);
+                numOfPredictions.push_back(0);
+                numOfMissPredictions.push_back(0);
+            }
             // init the buffer with the given init value
             for (int i = 0; i < bufferSize; i++)
                 buffer.push_back(taken);
 
             // init the historyBits with the given init value
-            for (int i = 0; i < m; i++) {
-                historyBits.push_back(taken);
-                allHistoryBits.push_back(taken);
+            if (m > 0) {
+                for (int i = 0; i < m; i++) {
+                    historyBits.push_back(taken);
+                    //allHistoryBits.push_back(taken);
+                }
             }
+            else 
+                historyBits.push_back(taken);
         }
 
         bool predict(bool actualTaken, int numOfBranch) {
-            int index = (numOfBranch-1) * 2;
+            int numBranchIndex = numOfBranch - 1;
+            int index = (bufferSizeForEachBranch * numOfBranch)-1;
             if (m > 0) {
+                index = (bufferSizeForEachBranch * (numOfBranch-1)); // calculate index of each branch buffer index
+
                 // covert historybit to binary and int to look up buffer
                 string binary = "";
                 for (int i = 0; i < m; i++) {
@@ -56,14 +66,15 @@ class DynamicBranchPredictor {
             // check buffer
             bool predictTaken = buffer[index];
 
-            cout << "Branch predict to be " << predictTaken << endl;
+            cout << "Branch " << numOfBranch << " predict to be " << predictTaken << endl;
 
-            numOfPredictions++;
+            numOfPredictions[numBranchIndex]++;
+            
 
             if (predictTaken)
-                numOfTakenBranches++;
+                numOfTakenBranches[numBranchIndex]++;
             else
-                numOfNotTakenBranches++;
+                numOfNotTakenBranches[numBranchIndex]++;
 
             int max = (pow(2, n) - 1);
             if (nbitCounter < max && nbitCounter < 0)
@@ -76,7 +87,7 @@ class DynamicBranchPredictor {
                 
             } 
             else { // miss prediction
-                numOfMissPredictions++;
+                numOfMissPredictions[numBranchIndex]++;
                 
                 // flip
                 if(nbitCounter >= (max/2))
@@ -87,73 +98,154 @@ class DynamicBranchPredictor {
 
             // add to history bit
             historyBits.push_back(actualTaken);
-            allHistoryBits.push_back(actualTaken);
+            //allHistoryBits.push_back(actualTaken);
 
             historyBits.pop_front();
+            
 
             return predictTaken;
         }
 
         void printStat() {
-            double accuracy = 100-(((double)numOfMissPredictions / numOfPredictions)*100);
-            cout << "========== Statistic ==========" << endl 
-                << "Number of Taken Branches: " << numOfTakenBranches << endl
-                << "Number of Not Taken Branches: " << numOfNotTakenBranches << endl
-                << "Number of Predictions: " << numOfPredictions << endl
-                << "Number of Missed Predictions: " << numOfMissPredictions << endl
-                << "Accuracy: " << accuracy << endl
-                << "===============================" << endl;
+            double accuracy;
+            int totalNumOfTakenBranches = 0;
+            int totalNumOfNotTakenBranches = 0;
+            int totalNumOfPredictions = 0; 
+            int totalNumOfMissPredictions = 0;
+            cout << "========== Statistic ==========" << endl;
+            for (int i = 0; i < numOfBranch; i++) {
+                accuracy = 100 - (((double)numOfMissPredictions[i] / numOfPredictions[i]) * 100);
+                cout << "========== Branch " << (i+1) << " ==========" << endl
+                    << "Number of Taken Branches: " << numOfTakenBranches[i] << endl
+                    << "Number of Not Taken Branches: " << numOfNotTakenBranches[i] << endl
+                    << "Number of Predictions: " << numOfPredictions[i] << endl
+                    << "Number of Missed Predictions: " << numOfMissPredictions[i] << endl
+                    << "Accuracy: " << accuracy << endl;
+
+                totalNumOfTakenBranches += numOfTakenBranches[i];
+                totalNumOfNotTakenBranches += numOfNotTakenBranches[i];
+                totalNumOfPredictions += numOfPredictions[i];
+                totalNumOfMissPredictions += numOfMissPredictions[i];
+            } 
+            accuracy = 100 - (((double)totalNumOfMissPredictions / totalNumOfPredictions) * 100);
+            cout << "========== Total ==========" << endl
+                << "Number of Taken Branches: " << totalNumOfTakenBranches << endl
+                << "Number of Not Taken Branches: " << totalNumOfNotTakenBranches << endl
+                << "Number of Predictions: " << totalNumOfPredictions << endl
+                << "Number of Missed Predictions: " << totalNumOfMissPredictions << endl
+                << "Accuracy: " << accuracy << endl;
+            cout << "===============================" << endl;
         }
         
         void printBuffer() {
             cout << "========== Buffer ==========" << endl;
-            for (int i = 0; i < bufferSize; i++)
+            for (int i = 0; i < bufferSize; i++) {
+                if (i == (bufferSizeForEachBranch - 1))
+                    cout << "Buffer " << ((i+1)/bufferSizeForEachBranch) << endl;
                 cout << buffer[i] << endl;
+            }
+                
         }
 
     private:
         int m, n, bufferSize, numOfBranch, bufferSizeForEachBranch, nbitCounter;
         deque<bool> buffer, historyBits, allHistoryBits;
 
-        int numOfTakenBranches, numOfNotTakenBranches, numOfPredictions, numOfMissPredictions;
+        deque<int> numOfTakenBranches, numOfNotTakenBranches, numOfPredictions, numOfMissPredictions;
 };
 
-int main()
+////////////////////////////////////////////////////////////////////////////////
+
+//Swap function definition to swap two elements
+void swap(int* x, int* y)
 {
-    DynamicBranchPredictor DynamicBranchPredictor(3,1,1);
-    DynamicBranchPredictor.init(false);
-    DynamicBranchPredictor.printBuffer();
-    /*
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(true, 1);
-    DynamicBranchPredictor.printBuffer();
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
 
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(true, 1);
-    DynamicBranchPredictor.printBuffer();
-    */
 
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(true, 1);
-    DynamicBranchPredictor.printBuffer();
+//Function definition for quick sort iterative method
+void quickSort(int arr[], int lowest, int highest, DynamicBranchPredictor &dynamicBranchPredictor)
+{
+    dynamicBranchPredictor.printBuffer();
+    // Create a new array as stack
+    vector<int> stack;
+    for (int i = 0; i < (highest - lowest + 1); i++)
+        stack.push_back(0);
 
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(true, 1);
-    DynamicBranchPredictor.printBuffer();
+    // Initializing top of stack as -1
+    int top = -1;
 
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(false, 1);
-    DynamicBranchPredictor.predict(true, 1);
-    DynamicBranchPredictor.printBuffer();
+    //Pushing the values of highest and lowest on stack
+    stack[++top] = lowest;
+    stack[++top] = highest;
 
-    DynamicBranchPredictor.printStat();
+
+
+    // Keep popping from stack while is not empty
+    while (top >= 0) { // branch 1
+
+        dynamicBranchPredictor.predict(true, 1);
+
+        //Popping the values of highest and lowest from stack 
+        highest = stack[top--];
+        lowest = stack[top--];
+
+        int x = arr[highest];
+        int i = (lowest - 1);
+
+        //For loop & swap function to set the pivot element at its correct position in an array
+        for (int j = lowest; j <= highest - 1; j++) { // branch 2
+            dynamicBranchPredictor.predict(true, 2);
+            if (arr[j] <= x) { // branch 3 
+                i++;
+                swap(&arr[i], &arr[j]);
+                dynamicBranchPredictor.predict(true, 3);
+            }
+            dynamicBranchPredictor.predict(false, 3);
+        }
+        dynamicBranchPredictor.predict(false, 2);
+        swap(&arr[i + 1], &arr[highest]);
+        int pivot = (i + 1);
+
+
+        //Pushing elements of left side of pivot to let side of stack
+        for (int i = 0; pivot - 1 > lowest && i == 0; i++) { // branch 4
+            dynamicBranchPredictor.predict(true, 4);
+            stack[++top] = lowest;
+            stack[++top] = pivot - 1;
+        }
+        dynamicBranchPredictor.predict(false, 4);
+
+        //Pushing elements of right side of pivot to right side of stack
+        for (int i = 0; pivot + 1 < highest && i == 0; i++) { // branch 5
+            dynamicBranchPredictor.predict(true, 5);
+            stack[++top] = pivot + 1;
+            stack[++top] = highest;
+        }
+        dynamicBranchPredictor.predict(false, 5);
+    }
+    dynamicBranchPredictor.predict(false, 1);
+}
+
+// Function to print the array
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+}
+
+int main()
+{    
+    DynamicBranchPredictor dynamicBranchPredictor(3, 1, 5);
+    dynamicBranchPredictor.init(false);
+    
+    int arr[] = { 15,7,1,3,12,17,5,13,14,100,19,9,50,8,80,2,16,4,10,11,60,15,10,1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr,0,size-1,dynamicBranchPredictor);
+    dynamicBranchPredictor.printStat();
+
+    printArray(arr, size);
+    
 }
